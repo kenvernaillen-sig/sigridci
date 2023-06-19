65fd03cb78fc194e424d35e19113e66529527477
Bruno Oliveira <olivbruno8@gmail.com>
2023-06-19 13:18:17 +0200

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,20 +1,36 @@
 function toggleAccordion(element) {
-    var item = element.classList.contains('menu-header') ? 
-        element.parentNode : element.parentNode.parentNode;
-
     var items = Array.from(document.querySelectorAll('.menu-item'));
-    items.forEach(function(i) {
-        if (item !== i) {
-            i.classList.remove('active');
-            i.querySelector('.menu-content').style.maxHeight = null;
-        }
-    });
 
-    if (item.classList.contains('active')) {
-        item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
-    } else {
-        item.classList.add('active');
-        item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+    // Check if the clicked element is a page.
+    if (element.classList.contains('page')) {
+        var item = element.parentNode.parentNode; // item is the grandparent
+        items.forEach(function(i) {
+            if (item !== i) { // collapse all other sections
+                i.classList.remove('active');
+                i.querySelector('.menu-content').style.maxHeight = null;
+            }
+        });
+
+        if (!item.classList.contains('active')) { // if the parent section is not active, make it active
+            item.classList.add('active');
+            item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+        }
+    } else { // if the clicked element is a header, toggle as before
+        var item = element.parentNode;
+        items.forEach(function(i) {
+            if (item !== i) {
+                i.classList.remove('active');
+                i.querySelector('.menu-content').style.maxHeight = null;
+            }
+        });
+
+        if (item.classList.contains('active')) {
+            item.querySelector('.menu-content').style.maxHeight = null;
+            item.classList.remove('active');
+        } else {
+            item.classList.add('active');
+            item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+        }
     }
 }
 
