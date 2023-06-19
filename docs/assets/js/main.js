aea577ec2c44b38bc3ff505a7bf379b7c79ed136
Bruno Oliveira <olivbruno8@gmail.com>
2023-06-19 13:07:05 +0200

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,29 +1,34 @@
 function toggleAccordion(element) {
-    var content = element.nextElementSibling;
-    var header = element;
+    var content;
+    var header;
     
     // Check if a sub-header was clicked
     if (element.classList.contains('page')) {
         content = element.parentElement; // The parent div.menu-content
         header = content.previousElementSibling; // The preceding div.menu-header
+    } else {
+        content = element.nextElementSibling;
+        header = element;
     }
     
     // Hide all menu contents
     var allMenuContents = document.querySelectorAll('#accordionMenu .menu-item .menu-content');
     allMenuContents.forEach(function(menuContent) {
         // Don't hide the clicked section or its parent section
-        if (menuContent !== content && menuContent.parentElement !== content.parentElement) {
+        if (menuContent !== content) {
             menuContent.style.maxHeight = null;
             menuContent.previousElementSibling.classList.remove('active');
         }
     });
     
-    // Show or hide the clicked menu content
-    if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-        header.classList.remove('active');
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-        header.classList.add('active');
+    // Show or hide the clicked menu content, unless a sub-header was clicked
+    if (!element.classList.contains('page')) {
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+            header.classList.remove('active');
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            header.classList.add('active');
+        }
     }
 }
