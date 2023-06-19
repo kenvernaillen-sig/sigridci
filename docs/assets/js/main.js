0b6e3f70a3d27f11e5640016f549f97fc51f140a
Bruno Oliveira <olivbruno8@gmail.com>
2023-06-19 13:21:34 +0200

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,40 +1,43 @@
 function toggleAccordion(element) {
     var items = Array.from(document.querySelectorAll('.menu-item'));
-
-    // Check if the clicked element is a page.
-    if (element.classList.contains('page')) {
-        var item = element.parentNode.parentNode; // item is the grandparent
-        items.forEach(function(i) {
-            if (item !== i) { // collapse all other sections
-                i.classList.remove('active');
-                i.querySelector('.menu-content').style.maxHeight = null;
-            }
-        });
-
-        if (!item.classList.contains('active')) { // if the parent section is not active, make it active
-            item.classList.add('active');
-            item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+    var item = element.parentNode;
+    items.forEach(function(i) {
+        if (item !== i) {
+            i.classList.remove('active');
+            i.querySelector('.menu-content').style.maxHeight = null;
         }
-    } else { // if the clicked element is a header, toggle as before
-        var item = element.parentNode;
-        items.forEach(function(i) {
-            if (item !== i) {
-                i.classList.remove('active');
-                i.querySelector('.menu-content').style.maxHeight = null;
-            }
-        });
+    });
 
-        if (item.classList.contains('active')) {
-            item.querySelector('.menu-content').style.maxHeight = null;
-            item.classList.remove('active');
-        } else {
-            item.classList.add('active');
-            item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+    if (item.classList.contains('active')) {
+        item.querySelector('.menu-content').style.maxHeight = null;
+        item.classList.remove('active');
+    } else {
+        item.classList.add('active');
+        item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
+    }
+}
+
+function highlightPage(element) {
+    var items = Array.from(document.querySelectorAll('.menu-item'));
+    var item = element.parentNode.parentNode; // item is the grandparent
+    items.forEach(function(i) {
+        if (item !== i) { // collapse all other sections
+            i.classList.remove('active');
+            i.querySelector('.menu-content').style.maxHeight = null;
         }
+    });
+
+    if (!item.classList.contains('active')) { // if the parent section is not active, make it active
+        item.classList.add('active');
+        item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
     }
 }
 
 // Bind the function to both header and page clicks
-document.querySelectorAll('.menu-header, .page').forEach(function(element) {
+document.querySelectorAll('.menu-header').forEach(function(element) {
     element.addEventListener('click', function() { toggleAccordion(element); });
+});
+
+document.querySelectorAll('.page').forEach(function(element) {
+    element.addEventListener('click', function() { highlightPage(element); });
 });
