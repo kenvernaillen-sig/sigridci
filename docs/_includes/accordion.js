ddaa4acf514cc289cb26a8f96f545d68b97e1664
Bruno Oliveira <olivbruno8@gmail.com>
2023-06-19 09:29:18 +0200

    Update accordion.js

--- docs/_includes/accordion.js
+++ docs/_includes/accordion.js
@@ -1,3 +1,32 @@
+/* JavaScript */
+// Get all accordion headers
+var acc = document.getElementsByClassName("accordion-header");
+
+for (var i = 0; i < acc.length; i++) {
+    // When an accordion header is clicked
+    acc[i].addEventListener("click", function() {
+        // Remove active class from other headers and content
+        var active = document.querySelector(".accordion .active");
+        if (active && active != this) {
+            active.classList.remove("active");
+            var activeContent = active.nextElementSibling;
+            activeContent.style.maxHeight = 0;
+        }
+
+        // Toggle active class
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        content.classList.toggle("active");
+        if (content.style.maxHeight) {
+            // If content is open, close it
+            content.style.maxHeight = null;
+        } else {
+            // If content is closed, open it
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
 // Get all main page links
 var mainLinks = document.getElementsByClassName("main-link");
 
@@ -7,7 +36,7 @@ for (var i = 0; i < mainLinks.length; i++) {
         e.preventDefault();
 
         // Remove active class from other headers and content
-        var active = document.querySelector(".toc .active");
+        var active = document.querySelector(".accordion .active");
         if (active) {
             active.classList.remove("active");
             var activeContent = active.nextElementSibling;
