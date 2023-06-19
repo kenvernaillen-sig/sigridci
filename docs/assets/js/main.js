dcba42005e901dbd84c1e5fd5117b5e81afbe9e7
Bruno Oliveira <olivbruno8@gmail.com>
2023-06-19 13:11:39 +0200

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,34 +1,24 @@
 function toggleAccordion(element) {
-    var content;
-    var header;
-    
-    // Check if a sub-header was clicked
-    if (element.classList.contains('page')) {
-        content = element.parentElement; // The parent div.menu-content
-        header = content.previousElementSibling; // The preceding div.menu-header
-    } else {
-        content = element.nextElementSibling;
-        header = element;
-    }
-    
-    // Hide all menu contents
-    var allMenuContents = document.querySelectorAll('#accordionMenu .menu-item .menu-content');
-    allMenuContents.forEach(function(menuContent) {
-        // Don't hide the clicked section or its parent section
-        if (menuContent !== content) {
-            menuContent.style.maxHeight = null;
-            menuContent.previousElementSibling.classList.remove('active');
+    var item = element.parentNode;
+
+    var items = Array.from(document.querySelectorAll('.menu-item'));
+    items.forEach(function(i) {
+        if (item !== i) {
+            i.classList.remove('active');
+            i.querySelector('.menu-content').style.maxHeight = null;
         }
     });
-    
-    // Show or hide the clicked menu content, unless a sub-header was clicked
-    if (!element.classList.contains('page')) {
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-            header.classList.remove('active');
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            header.classList.add('active');
-        }
+
+    if (item.classList.contains('active')) {
+        item.classList.remove('active');
+        item.querySelector('.menu-content').style.maxHeight = null;
+    } else {
+        item.classList.add('active');
+        item.querySelector('.menu-content').style.maxHeight = item.querySelector('.menu-content').scrollHeight + "px";
     }
 }
+
+// Bind the function to both header and page clicks
+document.querySelectorAll('.menu-header, .page').forEach(function(element) {
+    element.addEventListener('click', function() { toggleAccordion(element); });
+});
